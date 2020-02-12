46fd8dd9256788ee42a998e09bbcbf5c7a5aa953
Diego Rivera <diegoriveratapia13@gmail.com>
2020-02-12 14:09:05 -0400

    Mi segundo commit!!!

---
+++ Hello Word/helloword.java
@@ -0,0 +1,7 @@
+Hola mundo desde JAVA!!!
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
