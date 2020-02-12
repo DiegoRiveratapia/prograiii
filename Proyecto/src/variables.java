8f962e81fec7bac493e95b752a75b7dc391e10e0
Diego Rivera <diegoriveratapia13@gmail.com>
2020-02-12 15:09:05 -0400

    Manejo de variables printline

---
+++ Proyecto/src/variables.java
@@ -0,0 +1,22 @@
+public class variables {
+    public static void main(String[] args) {
+        int edad = 20;
+        int hora = 300;
+        String primer = " Mi primer dia";
+        String java = " Programa en java";
+        String password = "123456PWD";
+        System.out.println(edad+" "+password);
+        System.out.printf("la variable edad: %d es un numero y la hora es: %d \n",edad,hora);
+
+        System.out.printf("\n%s\n%s \n",primer,java);
+
+        int manzanas = 6;
+        int personas = 3;
+        int result = manzanas/personas;
+
+        System.out.printf("Si tengo %d manzanas y %d personas a cada persona le toca a %d manzanas",manzanas,personas,result);
+
+
+
+    }
+}
