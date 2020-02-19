d059388535e2851ccac00b5a3a546d7a34ea7483
Diego Rivera <diegoriveratapia13@gmail.com>
2020-02-19 15:04:35 -0400

    Introduccion a manejo de objetos

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -1,6 +1,11 @@
+import introduccion.Ejercicios;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hola mundo desde JAVA");
+        Ejercicios ejercicios = new Ejercicios();
+        ejercicios.seriesNaturales(20);
+        ejercicios.compara2cadenas("hola","hola");
+        ejercicios.cadena(15);
 
     }
 
