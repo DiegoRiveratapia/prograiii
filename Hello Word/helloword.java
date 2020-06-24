8bde80f162916c381d4392760083cb2dfd14fb74
Diego Rivera <diegoriveratapia13@gmail.com>
2020-06-24 00:43:49 -0400

    Ordenado

--- Hello Word/helloword.java
+++
@@ -1,7 +0,0 @@
-Hola mundo desde JAVA!!!
-public class HelloWorld {
-    public static void main(String[] args) {
-        // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
-    }
-}
